9696a87852648e6d585815f78e8e93abd4846b57
Jon Kurinsky <jkurinsky@gmail.com>
2019-12-08 16:04:56 -0500

    init commit: next and tailwind config

---
+++ components/layout.js
@@ -0,0 +1,7 @@
+import '../styles/main.css';
+
+const Layout = props => {
+  return <React.Fragment>{props.children}</React.Fragment>;
+};
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Index = () => {
+  return (
+    <Layout>
+      <div className="text-red-500 leading-normal">testing</div>
+    </Layout>
+  );
+};
+
+export default Index;
