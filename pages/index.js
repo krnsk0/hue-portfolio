6e362d118ac0f106e9d901f56efb3b7f18033d2d
krnsk0 <jkurinsky@gmail.com>
2020-02-15 11:43:15 -0500

    add sample images

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,13 @@ import Layout from '../components/layout';
 const Index = () => {
   return (
     <Layout>
-      <div className="text-red-500 leading-normal">testing</div>
-      <img src="/logo.png" alt="logo with text HUE art"></img>
+      <div>
+        <img src="/logo.png" alt="logo with text HUE art"></img>
+        <div className="text-black-500 leading-normal">hue art</div>
+        <div className="text-black-500 leading-normal">line 1</div>
+        <div className="text-black-500 leading-normal">line 2</div>
+      </div>
+      <div></div>
     </Layout>
   );
 };
