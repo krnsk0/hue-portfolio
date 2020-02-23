8707055fa6640f1afdc76ed5bac291ab25440c07
krnsk0 <jkurinsky@gmail.com>
2020-02-22 19:58:48 -0500

    basic flex setup for main

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -2,14 +2,17 @@ import styles from './styles';
 
 const Main = () => {
   return (
-    <>
+    <div css={styles.mainContainer}>
       <img src="/1.png" alt="logo with text HUE art" css={styles.image}></img>
+      <img src="/9.png" alt="logo with text HUE art" css={styles.image}></img>
       <img src="/2.png" alt="logo with text HUE art" css={styles.image}></img>
-      <img src="/3.png" alt="logo with text HUE art" css={styles.image}></img>
-      <img src="/4.png" alt="logo with text HUE art" css={styles.image}></img>
-      <img src="/5.png" alt="logo with text HUE art" css={styles.image}></img>
       <img src="/6.png" alt="logo with text HUE art" css={styles.image}></img>
-    </>
+      <img src="/4.png" alt="logo with text HUE art" css={styles.image}></img>
+      <img src="/8.png" alt="logo with text HUE art" css={styles.image}></img>
+      <img src="/3.png" alt="logo with text HUE art" css={styles.image}></img>
+      <img src="/5.png" alt="logo with text HUE art" css={styles.image}></img>
+      <img src="/7.png" alt="logo with text HUE art" css={styles.image}></img>
+    </div>
   );
 };
 
--- components/Main/styles.js
+++ components/Main/styles.js
@@ -2,8 +2,34 @@ import { css } from '@emotion/core';
 import { mediaQuery } from '../../emotion';
 
 export default {
-  image: css`
+  mainContainer: css`
+    display: flex;
+    flex-direction: column;
+    & > img:first-child {
+      margin-top: 0 !important;
+    }
+    & > img:last-child {
+      margin-bottom: 0 !important;
+    }
+
     ${mediaQuery('md')} {
+      flex-wrap: wrap;
+      max-height: 225vh;
+      & > img:first-child {
+        margin-top: 0.5em !important;
+      }
+      & > img:last-child {
+        margin-bottom: 0.5em !important;
+      }
+    }
+  `,
+  image: css`
+    margin: 0.5em 0.5em 0 0.5em;
+    width: calc(100% - 1em);
+
+    ${mediaQuery('md')} {
+      max-width: calc(50% - 1em);
+      margin: 0.5em 0.5em 0 0;
     }
   `,
 };
