077f4791f389cae07808d169a1640ef99f129a65
krnsk0 <jkurinsky@gmail.com>
2020-02-22 20:13:36 -0500

    switch to css column-based layout

--- components/Main/styles.js
+++ components/Main/styles.js
@@ -3,33 +3,17 @@ import { mediaQuery } from '../../emotion';
 
 export default {
   mainContainer: css`
-    display: flex;
-    flex-direction: column;
-    & > img:first-child {
-      margin-top: 0 !important;
+    ${mediaQuery('sm')} {
+      column-count: 2;
+      column-width: 50%;
     }
-    & > img:last-child {
-      margin-bottom: 0 !important;
-    }
-
-    ${mediaQuery('md')} {
-      flex-wrap: wrap;
-      max-height: 225vh;
-      & > img:first-child {
-        margin-top: 0.5em !important;
-      }
-      & > img:last-child {
-        margin-bottom: 0.5em !important;
-      }
+    ${mediaQuery('xl')} {
+      column-count: 3;
+      column-width: 33%;
     }
   `,
   image: css`
-    margin: 0.5em 0.5em 0 0.5em;
-    width: calc(100% - 1em);
-
-    ${mediaQuery('md')} {
-      max-width: calc(50% - 1em);
-      margin: 0.5em 0.5em 0 0;
-    }
+    margin: 0 0 0 0;
+    width: 100%;
   `,
 };
